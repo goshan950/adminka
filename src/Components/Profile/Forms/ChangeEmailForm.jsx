3e489c8ee2c1ddd663fa1895221d36935c7344ec
yury_mac <polyantsevyury@gmail.com>
2020-06-21 16:36:42 +0400

    Styles refactoring

--- src/Components/Profile/Forms/ChangeEmailForm.jsx
+++ src/Components/Profile/Forms/ChangeEmailForm.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Form, Input, Button } from 'antd';
+import { Form, Input} from 'antd';
 import {LightButton} from "../../common/Buttons/CustomButtons";
 
 const layout = {
--- src/Components/Tokens/Tokens.jsx
+++ src/Components/Tokens/Tokens.jsx
@@ -1,54 +1,52 @@
 import React, {Component} from 'react';
 import s from './Tokens.module.css';
-import {Badge, Input, Button, Card} from "antd";
+import {Badge, Input, Button} from "antd";
 import {BaseCard} from "../common/Blocks/CustomCards";
 
 class Tokens extends Component {
     render() {
         return (
             <div className={s.container}>
-                <div className={s.block}>
-                    <BaseCard title="Токены">
-                        <div className={s.content}>
-                            <div className={s.token}>
-                                <img src='https://sms-activate.ru/assets/img/logoBlack.png?1' alt='#'/>
-                                <Badge count={<Button size="small">100р.</Button>} offset={[0, -5]}>
-                                    <Badge count={<Button style={{backgroundColor: '#e6f7ff'}} size="small">+</Button>}
-                                           offset={[0, 30]}>
-                                        <Input/>
-                                    </Badge>
+                <BaseCard title="Токены" styles={{maxWidth: 800, flexGrow: 1}}>
+                    <div className={s.tokenContainer}>
+                        <div className={s.tokenItem}>
+                            <img src='https://sms-activate.ru/assets/img/logoBlack.png?1' alt='#'/>
+                            <Badge count={<Button size="small">100р.</Button>} offset={[0, -5]}>
+                                <Badge count={<Button style={{backgroundColor: '#e6f7ff'}} size="small">+</Button>}
+                                       offset={[0, 30]}>
+                                    <Input/>
                                 </Badge>
-                            </div>
-                            <div className={s.token}>
-                                <img src='https://smshub.org/assets/ico/logo.png' alt='#'/>
-                                <Badge count={<Button size="small">550р.</Button>} offset={[0, -5]}>
-                                    <Badge count={<Button style={{backgroundColor: '#e6f7ff'}} size="small">+</Button>}
-                                           offset={[0, 30]}>
-                                        <Input/>
-                                    </Badge>
-                                </Badge>
-                            </div>
-                            <div className={s.token}>
-                                <img src='https://sms-activate.ru/assets/img/logoBlack.png?1' alt='#'/>
-                                <Badge count={<Button size="small">10р.</Button>} offset={[0, -5]}>
-                                    <Badge count={<Button style={{backgroundColor: '#e6f7ff'}} size="small">+</Button>}
-                                           offset={[0, 30]}>
-                                        <Input/>
-                                    </Badge>
-                                </Badge>
-                            </div>
-                            <div className={s.token}>
-                                <img src='https://smshub.org/assets/ico/logo.png' alt='#'/>
-                                <Badge count={<Button size="small">1000р.</Button>} offset={[0, -5]}>
-                                    <Badge count={<Button style={{backgroundColor: '#e6f7ff'}} size="small">+</Button>}
-                                           offset={[0, 30]}>
-                                        <Input/>
-                                    </Badge>
-                                </Badge>
-                            </div>
+                            </Badge>
                         </div>
-                    </BaseCard>
-                </div>
+                        <div className={s.tokenItem}>
+                            <img src='https://smshub.org/assets/ico/logo.png' alt='#'/>
+                            <Badge count={<Button size="small">550р.</Button>} offset={[0, -5]}>
+                                <Badge count={<Button style={{backgroundColor: '#e6f7ff'}} size="small">+</Button>}
+                                       offset={[0, 30]}>
+                                    <Input/>
+                                </Badge>
+                            </Badge>
+                        </div>
+                        <div className={s.tokenItem}>
+                            <img src='https://sms-activate.ru/assets/img/logoBlack.png?1' alt='#'/>
+                            <Badge count={<Button size="small">10р.</Button>} offset={[0, -5]}>
+                                <Badge count={<Button style={{backgroundColor: '#e6f7ff'}} size="small">+</Button>}
+                                       offset={[0, 30]}>
+                                    <Input/>
+                                </Badge>
+                            </Badge>
+                        </div>
+                        <div className={s.tokenItem}>
+                            <img src='https://smshub.org/assets/ico/logo.png' alt='#'/>
+                            <Badge count={<Button size="small">1000р.</Button>} offset={[0, -5]}>
+                                <Badge count={<Button style={{backgroundColor: '#e6f7ff'}} size="small">+</Button>}
+                                       offset={[0, 30]}>
+                                    <Input/>
+                                </Badge>
+                            </Badge>
+                        </div>
+                    </div>
+                </BaseCard>
             </div>
         );
     }
