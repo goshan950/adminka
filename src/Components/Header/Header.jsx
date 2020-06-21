72cbf78bf519042262235e5c831aa1f43c0f2fb6
yury_mac <polyantsevyury@gmail.com>
2020-06-21 22:14:04 +0400

    Header refactoring

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -55,8 +55,9 @@ class Header extends Component {
     };
 
     setModal1Visible(modal1Visible) {
-        this.setState({ modal1Visible });
+        this.setState({modal1Visible});
     }
+
     //Modal number
     toggleCollapsed = () => {
         this.props.toggleNavCollapsed(!this.props.navCollapsed);
@@ -65,60 +66,56 @@ class Header extends Component {
     render() {
         return (
             <header className={s.header}>
-                <div className={s.container}>
-                    <span onClick={this.toggleCollapsed} className={s.collapseContainer}>
-                        {React.createElement(this.props.navCollapsed ? MenuUnfoldOutlined : MenuFoldOutlined)}
-                    </span>
-                    <img className={s.logo}
-                         src='https://kopeechka.store/tpl/panel/img/logo.svg' alt='#'/>
-                    <div className={s.number}>
-                        <Button block onClick={() => this.setModal1Visible(true)}>Получить номер</Button>
-                    </div>
-                    <Modal
-                        style={{top: 20}}
-                        okText={'Оплатить'}
-                        cancelText={'Отмена'}
-                        visible={this.state.modal1Visible}
-                        onOk={() => this.setModal1Visible(false)}
-                        onCancel={() => this.setModal1Visible(false)}
+                <span onClick={this.toggleCollapsed} className={s.collapseBlock}>
+                    {React.createElement(this.props.navCollapsed ? MenuUnfoldOutlined : MenuFoldOutlined)}
+                </span>
+                <img className={s.logo}
+                     src='https://kopeechka.store/tpl/panel/img/logo.svg' alt='#'/>
+                <div className={s.number}>
+                    <Button block onClick={() => this.setModal1Visible(true)}>Получить номер</Button>
+                </div>
+                <Modal
+                    style={{top: 20}}
+                    okText={'Оплатить'}
+                    cancelText={'Отмена'}
+                    visible={this.state.modal1Visible}
+                    onOk={() => this.setModal1Visible(false)}
+                    onCancel={() => this.setModal1Visible(false)}
+                >
+                    <Input placeholder={'Введите домен'} style={{marginTop: 20}}/>
+                    <Select
+                        showSearch
+                        style={{width: 200, marginTop: 20}}
+                        placeholder="Выберите страну"
+                        optionFilterProp="children"
+                        onChange={onChangeCountry}
+                        onFocus={onFocus}
+                        onBlur={onBlur}
+                        onSearch={onSearch}
+                        filterOption={(input, option) =>
+                            option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+                        }
                     >
-                        <Input placeholder={'Введите домен'} style={{marginTop: 20}}/>
-                        <Select
-                            showSearch
-                            style={{width: 200, marginTop: 20}}
-                            placeholder="Выберите страну"
-                            optionFilterProp="children"
-                            onChange={onChangeCountry}
-                            onFocus={onFocus}
-                            onBlur={onBlur}
-                            onSearch={onSearch}
-                            filterOption={(input, option) =>
-                                option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
-                            }
-                        >
-                            <Option value="russia">Россия</Option>
-                            <Option value="usa">США</Option>
-                            <Option value="itali">Италия</Option>
-                        </Select>
-                    </Modal>
-
-                    <div className={s.userContainer}>
-                        <Avatar shape="square" className={s.ava} size={48} icon={<UserOutlined />}/>
-                        <Dropdown overlay={
-                            <Menu>
-                                <Menu.Item key="1">
-                                    <Button type="link" onClick={showConfirm}>
-                                        Пополнить
-                                    </Button>
-                                </Menu.Item>
-                            </Menu>
-                        }>
+                        <Option value="russia">Россия</Option>
+                        <Option value="usa">США</Option>
+                        <Option value="itali">Италия</Option>
+                    </Select>
+                </Modal>
+                <div className={s.userContainer}>
+                    <Avatar shape="square" className={s.ava} size={48} icon={<UserOutlined/>}/>
+                    <Dropdown overlay={
+                        <Menu>
+                            <Menu.Item key="1">
+                                <Button type="link" onClick={showConfirm}>
+                                    Пополнить
+                                </Button>
+                            </Menu.Item>
+                        </Menu>
+                    }>
                             <span className={s.balance}>
-                                100 руб. <DownOutlined />
+                                100 руб. <DownOutlined/>
                             </span>
-                        </Dropdown>
-                    </div>
-
+                    </Dropdown>
                 </div>
             </header>
         );
